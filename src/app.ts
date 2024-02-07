49d39b1ab4f43447f438037a5612d55351d226f5
mahiuddinTuhin <mahiuddiinntuhin@gmail.com>
2024-02-07 11:29:07 +0600

    cors refact

--- src/app.ts
+++ src/app.ts
@@ -11,7 +11,7 @@ app.use(express.json());
 app.use(
   cors({
     credentials: true,
-    origin: ["https://booktook.netlify.app/sells"],
+    origin: ["https://booktook.netlify.app"],
   }),
 );
 
