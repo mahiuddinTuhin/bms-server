de7b6e25cfc93a1c502501e8942ddd35cc0f62dc
mahiuddinTuhin <mahiuddiinntuhin@gmail.com>
2024-02-07 11:20:01 +0600

    cors refact

--- src/app.ts
+++ src/app.ts
@@ -8,7 +8,9 @@ import APIv1Routes from "./app/routes";
 const app = express();
 
 app.use(express.json());
-app.use(cors({ credentials: true, origin: "http://localhost:5173" }));
+app.use(
+  cors({ credentials: true, origin: "https://silly-teal-crab.cyclic.app" }),
+);
 
 app.use("/api/v1", APIv1Routes);
 
